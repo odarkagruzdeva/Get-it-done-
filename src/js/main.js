da37c45a25834af7afd38e701ab9b58602e04310
Daryna <dashunya1@gmail.com>
2019-02-21 13:10:47 -0500

    last vers

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,38 @@
-p
+let toDoList = [
+    {
+      listItem: `Develop the concept based on conducted research for Chris`,
+      comState: false
+    },
+    
+    {
+      listItem: `Create personas and journey maps`,
+      comState: true,
+    },
+    
+    {
+      listItem: `Develop system maps`,
+      comState: false
+    },
+    
+    {
+      listItem: `Create wireframes for AR App`,
+      comState: true
+    },
+    
+    {
+      listItem: `Create video prototype of AR experience`,
+      comState: false
+    },
+    ];
+    
+    toDoList.forEach(itm {
+      let oneListItem = document.createElement(`li`);
+      oneListItem.innerHTML = itm.listItem;
+      oneListItem.classList.add(`listItem`);
+    
+      if (itm.comState == true)
+        oneListItem.classList.add (`complete`);]
+    
+        document.getElementById('toDoList').appendChild(oneListItem);
+    });
+    
