ad922d27876bebe6560bb9a6965b36ca1d9b2440
Daryna <dashunya1@gmail.com>
2019-02-21 13:45:12 -0500

    laaaaast

--- src/js/main.js
+++ src/js/main.js
@@ -25,13 +25,13 @@ let toDoList = [
 },
 ];
 
-toDoList.forEach(itm {
+toDoList.forEach(itm => {
   let oneListItem = document.createElement(`li`);
   oneListItem.innerHTML = itm.listItem;
   oneListItem.classList.add(`listItem`);
 
   if (itm.comState == true)
-    oneListItem.classList.add (`complete`);]
+    oneListItem.classList.add (`complete`);
 
     document.getElementById('toDoList').appendChild(oneListItem);
 });
